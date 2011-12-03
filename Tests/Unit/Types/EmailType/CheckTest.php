47a9a3287e004af72062d6ff97eb88ad34c6d8c5
Viktor Masicek <viktor@masicek.net>
2011-12-03 01:06:17 +0100

    Tests/Types/EmailType

---
+++ Tests/Unit/Types/EmailType/CheckTest.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * PhpOption
+ * @link git@github.com:masicek/PhpOptions.git
+ * @author Viktor Mašíček <viktor@masicek.net>
+ * @license "New" BSD License
+ */
+
+namespace Tests\PhpOptions\EmailType;
+
+use \Tests\PhpOptions\TestCase;
+use \PhpOptions\EmailType;
+
+require_once ROOT . '/Types/EmailType.php';
+
+/**
+ * CheckTest
+ *
+ * @author Viktor Mašíček <viktor@masicek.net>
+ *
+ * @covers PhpOptions\EmailType::check
+ */
+class CheckTest extends TestCase
+{
+
+
+	public function testOk()
+	{
+		$type = new EmailType();
+		$this->assertTrue($type->check('foo@bar.net'));
+	}
+
+
+	public function testBad()
+	{
+		$type = new EmailType();
+		$this->assertFalse($type->check('foo'));
+		$this->assertFalse($type->check('@bar.net'));
+		$this->assertFalse($type->check('foo@bar'));
+		$this->assertFalse($type->check('foo.net'));
+		$this->assertFalse($type->check('foo@.net'));
+		$this->assertFalse($type->check('foo@bar.'));
+	}
+
+
+}
